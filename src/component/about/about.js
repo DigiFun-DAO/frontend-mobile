a9bece7115ea347a61013cdf35b8c11decc16a86
garry <lugh60@gmail.com>
2021-12-28 18:04:31 +0800

    update about us, .gitignore

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -13,23 +13,25 @@ class About extends React.Component {
           <div className="product_content_words">
             <div className="product_content_words_title">About Us</div>
             <div className="subtitle">
-              Dress to EARN, Creative for FUN!
+              How did DigiFun start？
             </div>
             <div className="words">
-              Everyone can be a creator. Suggest your idea, vote for your favorite creative product and make them a
-              reality.Everyone can be a creator. Suggest your idea, vote for your favorite creative product and make
-              them a reality.Everyone can be a creator. Suggest your idea, vote for your favorite creative product and
-              make them a reality.Everyone can be a creator. Suggest your idea, vote for your favorite creative product
-              and make them a reality.Everyone can be a creator. Suggest your idea, vote for your favorite creative
-              product and make them a reality.
-            </div>
-            <div className="subtitle">
-              Dress to EARN, Creative for FUN!
+              DigiFun was founded in October 2021 by four partners who were passionate about blockchain technology and NFTs.
             </div>
             <div className="words">
-              Everyone can be a creator. Suggest your idea, vote for your favorite creative product and make them a
-              reality.Everyone can be a creator. Suggest your idea, vote for your favorite creative produ can be a
-              creator. Suggest your idea, vote for your favorite creative product and make them a reality.
+              DigiFun is a communication, production, and investment platform for digital entertainment in the Metaverse, managed in the form of a Decentralized Autonomous Organization (DAO). It’s a mechanism based on UGC to design more user autonomous products to promote its development.
+            </div>
+            <div className="words">
+              At first, DigiFun noticed that there was a large group of people who couldn't draw, didn't know how to model or other techniques, but they had a lot of interesting and creative ideas. DigiFunDAO was then established, aiming to produce best NFTs through joint collaborations. The outputs of DigiFun's NFTs are produced through the proposals and votes by community members, and they are the creations and results of community join efforts. DigiFun's products and community members will cover Decentraland, Sandbox and many other mainstreaming Metaverse platforms.
+            </div>
+            <div className="words">
+              DigiFun will expand different platform markets, serve more people with creative ideas, and establish a platform to display and sell NFTs. DigiFun is committed to exploring the possibilities of more cutting-edge technologies and digital articrafts in future, and working with all of you to build the digital future.
+            </div>
+            <div className="words">
+              For business or press enquiries：
+            </div>
+            <div className="words">
+              Contact us：official@digifundao.com
             </div>
           </div>
         </div>
