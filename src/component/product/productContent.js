4f6594bdcbb4abc372cd505cfcc648948873c74d
garry <lugh60@gmail.com>
2021-12-30 00:58:56 +0800

    dynamic products elements

--- src/component/product/productContent.js
+++ src/component/product/productContent.js
@@ -6,21 +6,41 @@ import Footer from "../footer";
 
 const products = [
   {
-    imgPath: "../../assets/digi/product1.png", title: "Evil Snowman",
-    source: "Polygon", prize: 100, class: "../assets/digi/class_dress.svg", sex: "../assets/digi/sex_male.svg"
+    imgPath: "product1.png",
+    title: "Evil Snowman",
+    url: "https://market.decentraland.org/contracts/0x622a009f894bf7c6ba069a31217be23f327e95de/items/0",
+    source: "Polygon",
+    prize: 100,
+    class: "class_head.png",
+    sex: "sex_all.svg"
   },
   {
-    imgPath: "../../assets/digi/product2.png", title: "Evil Snowman",
-    source: "Polygon", prize: 100, class: "../assets/digi/class_dress.svg", sex: "../assets/digi/sex_male.svg"
+    imgPath: "product2.png",
+    title: "Evil Snowman",
+    url: "https://market.decentraland.org/contracts/0x622a009f894bf7c6ba069a31217be23f327e95de/items/1",
+    source: "Polygon",
+    prize: 100,
+    class: "class_dress.svg",
+    sex: "sex_all.svg"
   },
   {
-    imgPath: "../../assets/digi/product3.png", title: "Evil Snowman",
-    source: "Polygon", prize: 100, class: "../assets/digi/class_dress.svg", sex: "../assets/digi/sex_male.svg"
+    imgPath: "product3.png",
+    title: "R·G YEARning",
+    url: "https://market.decentraland.org/contracts/0xc2c1efd454d4e8c5f91477f63abc53fae74d2ed3/items/0",
+    source: "Polygon",
+    prize: 100,
+    class: "class_dress.svg",
+    sex: "sex_male.svg"
   },
   {
-    imgPath: "../../assets/digi/product1.png", title: "Evil Snowman",
-    source: "Polygon", prize: 100, class: "../assets/digi/class_dress.svg", sex: "../assets/digi/sex_male.svg"
-  },
+    imgPath: "product4.png",
+    title: "R·G YEARning",
+    url: "https://market.decentraland.org/contracts/0xc2c1efd454d4e8c5f91477f63abc53fae74d2ed3/items/1",
+    source: "Polygon",
+    prize: 100,
+    class: "class_pants.png",
+    sex: "sex_male.svg"
+  }
 ]
 
 class ProductContent extends React.Component {
@@ -36,92 +56,28 @@ class ProductContent extends React.Component {
             </div>
           </div>
           <div className="product_card_frame">
-            {/*{products.map((item, idx) => (*/}
-            <a href="https://market.decentraland.org/contracts/0x622a009f894bf7c6ba069a31217be23f327e95de/items/0">
-              <div className="product_card">
-                <div className="product_content_img_frame">
-                  <img src={require('../../assets/product1.png').default} width="40%"/>
-                </div>
-                <div className="product_content_detail_comments">
-                  <div className="product_content_detail_comments_intro">
-                    <div className="product_content_detail_comments_intro_title">Evil Snowman</div>
-                    <div className="product_content_detail_comments_intro_source">Polygon</div>
-                    <img src={require('../../assets/class_head.png').default}
-                         className="product_content_detail_comments_intro_class"/>
-                    <img src={require('../../assets/sex_all.svg').default}
-                         className="product_content_detail_comments_intro_sex"/>
-                    <img src={require('../../assets/prize_icon.png').default}
-                         className="product_content_detail_prize_icon"/>
-                    <span className="product_content_detail_prize_words">100</span>
+            {products.map((item, idx) => (
+                <a href={item.url}>
+                  <div className="product_card">
+                    <div className="product_content_img_frame">
+                      <img src={require('../../assets/' + item.imgPath).default} width="40%"/>
+                    </div>
+                    <div className="product_content_detail_comments">
+                      <div className="product_content_detail_comments_intro">
+                        <div className="product_content_detail_comments_intro_title">{item.title}</div>
+                        <div className="product_content_detail_comments_intro_source">{item.source}</div>
+                        <img src={require('../../assets/' + item.class).default}
+                             className="product_content_detail_comments_intro_class"/>
+                        <img src={require('../../assets/' + item.sex).default}
+                             className="product_content_detail_comments_intro_sex"/>
+                        <img src={require('../../assets/prize_icon.png').default}
+                             className="product_content_detail_prize_icon"/>
+                        <span className="product_content_detail_prize_words">{item.prize}</span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </a>
-
-            <a href="https://market.decentraland.org/contracts/0x622a009f894bf7c6ba069a31217be23f327e95de/items/1">
-              <div className="product_card">
-                <div className="product_content_img_frame">
-                  <img src={require('../../assets/product2.png').default} width="50%"/>
-                </div>
-                <div className="product_content_detail_comments">
-                  <div className="product_content_detail_comments_intro">
-                    <div className="product_content_detail_comments_intro_title">Evil Snowman</div>
-                    <div className="product_content_detail_comments_intro_source">Polygon</div>
-                    <img src={require('../../assets/class_dress.svg').default}
-                         className="product_content_detail_comments_intro_class"/>
-                    <img src={require('../../assets/sex_all.svg').default}
-                         className="product_content_detail_comments_intro_sex"/>
-                    <img src={require('../../assets/prize_icon.png').default}
-                         className="product_content_detail_prize_icon"/>
-                    <span className="product_content_detail_prize_words">100</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-
-            <a href="https://market.decentraland.org/contracts/0xc2c1efd454d4e8c5f91477f63abc53fae74d2ed3/items/0">
-              <div className="product_card">
-                <div className="product_content_img_frame">
-                  <img src={require('../../assets/product3.png').default} width="80%"/>
-                </div>
-                <div className="product_content_detail_comments">
-                  <div className="product_content_detail_comments_intro">
-                    <div className="product_content_detail_comments_intro_title">R·G YEARning</div>
-                    <div className="product_content_detail_comments_intro_source">Polygon</div>
-                    <img src={require('../../assets/class_dress.svg').default}
-                         className="product_content_detail_comments_intro_class"/>
-                    <img src={require('../../assets/sex_male.svg').default}
-                         className="product_content_detail_comments_intro_sex"/>
-                    <img src={require('../../assets/prize_icon.png').default}
-                         className="product_content_detail_prize_icon"/>
-                    <span className="product_content_detail_prize_words">100</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-
-            <a href="https://market.decentraland.org/contracts/0xc2c1efd454d4e8c5f91477f63abc53fae74d2ed3/items/1">
-              <div className="product_card">
-                <div className="product_content_img_frame">
-                  <img src={require('../../assets/product4.png').default} width="60%"/>
-                </div>
-                <div className="product_content_detail_comments">
-                  <div className="product_content_detail_comments_intro">
-                    <div className="product_content_detail_comments_intro_title">R·G YEARning</div>
-                    <div className="product_content_detail_comments_intro_source">Polygon</div>
-                    <img src={require('../../assets/class_pants.png').default}
-                         className="product_content_detail_comments_intro_class"/>
-                    <img src={require('../../assets/sex_male.svg').default}
-                         className="product_content_detail_comments_intro_sex"/>
-                    <img src={require('../../assets/prize_icon.png').default}
-                         className="product_content_detail_prize_icon"/>
-                    <span className="product_content_detail_prize_words">100</span>
-                  </div>
-                </div>
-              </div>
-            </a>
-            {/*))}*/}
-
+                </a>
+              ))}
           </div>
         </div>
 
